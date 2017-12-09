1bc4a782433de36b661038bc279c852911743f20
Paweł Brzoza <brzozik95@outlook.com>
2017-12-09 17:18:24 +0100

    expert system

---
+++ BeerAdvisor/BeerAdvisor/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace BeerAdviser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "słodkie",
+            "gorzkie",
+            "gazowane",
+            "średniogazowane",
+            "duża piana",
+            "mała piana",
+            "ciemne",
+            "jasne",
+            "smakowe",
+            "tanie"});
+            this.checkedListBox1.Location = new System.Drawing.Point(68, 80);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(146, 214);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(323, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sprawdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lech",
+            "Lech Free",
+            "Lech Radler",
+            "Tyskie",
+            "Tyskie Radler",
+            "Perła",
+            "Gosser",
+            "Heineken",
+            "Żywiec",
+            "Żywiec Porter",
+            "Okocim",
+            "Okocim Porter",
+            "Carlsberg",
+            "Tatra",
+            "Harnaś"});
+            this.comboBox1.Location = new System.Drawing.Point(332, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(57, 361);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 204);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "wybierz cechy piwa które lubisz:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(301, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "wybierz ulubione piwo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "wszytkie cechy piwa które lubisz:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(305, 361);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(182, 204);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(286, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "piwa które mogą Ci smakować:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(308, 254);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(157, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "wszytkie zaznaczone cechy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(308, 277);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(184, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "chociaż jedną zaznaczoną ceche";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(302, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Wyświetlaj piwa które zawierają:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 605);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Beer Adviser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ BeerAdvisor/BeerAdvisor/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+
+namespace BeerAdviser
+{
+    public partial class Form1 : Form
+    {
+        List<Beer> allBeers = new List<Beer>();
+        List<string> checkedFeatures = new List<string>();
+        List<string> otherBeers = new List<string>();
+        string selectedBeer;
+
+        private class Beer
+        {
+            string name;
+            List<string> features;
+
+            public Beer(string name, List<string> features)
+            {
+                this.Name = name;
+                this.Features = features;
+            }
+
+            public string Name { get => name; set => name = value; }
+            public List<string> Features { get => features; set => features = value; }
+        }
+
+        public Form1()
+        {
+            allBeers.Add(new Beer("Lech",          new List<string>(new string[] { "słodkie", "gazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Lech Free",     new List<string>(new string[] { "słodkie", "średniogazowane", "mała piana", "jasne" })));
+            allBeers.Add(new Beer("Lech Radler",   new List<string>(new string[] { "słodkie", "średniogazowane", "mała piana", "jasne", "smakowe" })));
+            allBeers.Add(new Beer("Tyskie",        new List<string>(new string[] { "gorzkie", "gazowane", "mała piana", "jasne" })));
+            allBeers.Add(new Beer("Tyskie Radler", new List<string>(new string[] { "słodkie", "średniogazowane", "mała piana", "jasne", "smakowe" })));
+            allBeers.Add(new Beer("Perła",         new List<string>(new string[] { "słodkie", "gazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Gosser",        new List<string>(new string[] { "słodkie", "średniogazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Heineken",      new List<string>(new string[] { "słodkie", "średniogazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Żywiec",        new List<string>(new string[] { "gorzkie", "gazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Żywiec Porter", new List<string>(new string[] { "gorzkie", "gazowane", "duża piana", "ciemne" })));
+            allBeers.Add(new Beer("Okocim",        new List<string>(new string[] { "gorzkie", "gazowane", "duża piana", "jasne" })));
+            allBeers.Add(new Beer("Okocim Porter", new List<string>(new string[] { "gorzkie", "średniogazowane", "mała piana", "ciemne" })));
+            allBeers.Add(new Beer("Carlsberg",     new List<string>(new string[] { "gorzkie", "gazowane", "duża piana", "ciemne" })));
+            allBeers.Add(new Beer("Tatra",         new List<string>(new string[] { "gorzkie", "średniogazowane", "mała piana", "jasne", "tanie" })));
+            allBeers.Add(new Beer("Harnaś",        new List<string>(new string[] { "gorzkie", "średniogazowane", "mała piana", "jasne", "tanie" })));
+
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            checkedFeatures.Clear();
+            otherBeers.Clear();
+
+            //load data from comboboxa and checkedlist
+            selectedBeer = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+
+            foreach (var item in checkedListBox1.CheckedItems)
+                checkedFeatures.Add(item.ToString());
+
+            //checking and adding favourite beers features
+
+            addingSelectedBeerFeatures();
+
+            foreach (var xx in checkedFeatures)
+                listBox1.Items.Add(xx);
+
+            checkingInMyTypeBeers();
+
+        }
+
+        private void addingSelectedBeerFeatures()
+        {
+           
+            var myBeer = allBeers.Find(r => r.Name == selectedBeer);
+
+            if(myBeer != null)
+            {
+                bool state;
+                for (int j = 0; j < myBeer.Features.Count; j++)
+                {
+                    state = false;
+                    for (int i = 0; i < checkedFeatures.Count; i++)
+                    {
+                        if (checkedFeatures[i] == myBeer.Features[j])
+                            state = true;
+                    }
+                    if (state == false)
+                        checkedFeatures.Add(myBeer.Features[j]);
+                }
+            }
+        }
+
+        private void checkingInMyTypeBeers()
+        {
+
+            // adding beer on list if only one beer features is met
+
+            if(radioButton2.Checked == true)
+            {
+                bool state;
+                for (int i = 0; i < checkedFeatures.Count ; i++)
+                {
+                    for (int j = 0; j < allBeers.Count ; j++)
+                    {
+                        state = false;
+                        for (int z = 0; z < allBeers[j].Features.Count; z++)
+                        {
+                            if (checkedFeatures[i] == allBeers[j].Features[z])
+                                state = true;
+                        }
+
+                        if (state == true && listBox2.FindString(allBeers[j].Name) == -1)
+                            listBox2.Items.Add(allBeers[j].Name);
+                    }
+                }
+            }
+            else
+            {
+                // adding beer on list if all bear features are met
+
+                bool exist, allexist;
+
+                for (int j = 0; j < allBeers.Count; j++)
+                {
+                    allexist = true;
+                    for (int i = 0; i < checkedFeatures.Count; i++)
+                    {
+                        exist = false;
+                        for (int z = 0; z < allBeers[j].Features.Count; z++)
+                        {
+                            if (checkedFeatures[i] == allBeers[j].Features[z])
+                            {
+                                exist = true;
+                                break;
+                            }
+                        }
+                        if (exist == false)
+                            allexist = false;
+                    }
+                    if (allexist == true)
+                        listBox2.Items.Add(allBeers[j].Name);
+                }
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
